4e47a8d8e581e63215c5e1be105540c7e5e22bfa
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-15 17:51:48 -0600

    added sighnup page not functioning

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,81 @@
-export default function Signup() {
+// ... (other imports)
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+
+
+function Signup(props) {
+    // const [formState, setFormState] = useState({ email: '', password: '', username: '' });
+    // const [addUser] = useMutation(ADD_USER);
+  
+    // const handleFormSubmit = async (event) => {
+    //   event.preventDefault();
+    //   const mutationResponse = await addUser({
+    //     variables: {
+    //       email: formState.email,
+    //       password: formState.password,
+    //       username: formState.username,
+    //     },
+    //   });
+    //   const token = mutationResponse.data.addUser.token;
+    //   Auth.login(token);
+    // };
+  
+    // const handleChange = (event) => {
+    //   const { name, value } = event.target;
+    //   setFormState({
+    //     ...formState,
+    //     [name]: value,
+    //   });
+    // };
+  
     return (
-        <h1>signup</h1>
-    )
-}
+      <div className="container my-1">
+        <Link to="/login">← Go to Login</Link>
+  
+        <h2>Signup</h2>
+        <form>
+        {/* onSubmit={handleFormSubmit} */}
+      
+          
+          <div className="">
+            <label htmlFor="username">Username:</label>
+            <input
+              placeholder="YourUsername"
+              name="username"
+              type="text"
+              id="username"
+              // onChange={handleChange}
+            />
+          </div>
+          <div className="">
+            <label htmlFor="email">Email:</label>
+            <input
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              // onChange={handleChange}
+            />
+          </div>
+          <div className="">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              // onChange={handleChange}
+            />
+          </div>
+          <div className="">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Signup;
+  
