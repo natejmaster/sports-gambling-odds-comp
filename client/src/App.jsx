da4082a86d2e65577d7231f6bc2b4b9368f58b9c
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-14 21:08:08 -0600

    react init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <header>
+      <h1>Basic Title</h1>
+    </header>
+    </>
+  )
+}
+
+export default App
