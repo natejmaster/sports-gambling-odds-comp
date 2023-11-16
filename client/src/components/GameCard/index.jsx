90ca0cf0e599576896b2e40559f61775a54f0c28
natejmaster@gmail.com <natejmaster@gmail.com>
2023-11-15 18:31:34 -0600

    Got odds to display

--- client/src/components/GameCard/index.jsx
+++ client/src/components/GameCard/index.jsx
@@ -5,71 +5,83 @@ const GameCard = ({ homeTeam, awayTeam, bookmakers }) => {
   const [bestOdds, setBestOdds] = useState(null);
 
   useEffect(() => {
+    const findBestOdds = (oddsData) => {
+      const bestOdds = {
+        pointSpreadTeam1: null,
+        pointSpreadTeam2: null,
+        over: null,
+        under: null,
+        moneylineTeam1: null,
+        moneylineTeam2: null,
+      };
+
+      bookmakers.forEach((bookmaker) => {
+        bookmaker.markets.forEach((market) => {
+          switch (market.key) {
+            case 'h2h':
+              market.outcomes.forEach((outcome) => {
+                if (outcome.name === homeTeam && (bestOdds.moneylineTeam1 === null || outcome.price > bestOdds.moneylineTeam1)) {
+                  bestOdds.moneylineTeam1 = outcome.price;
+                } else if (outcome.name === awayTeam && (bestOdds.moneylineTeam2 === null || outcome.price > bestOdds.moneylineTeam2)) {
+                  bestOdds.moneylineTeam2 = outcome.price;
+                }
+              });
+              break;
+            case 'spreads':
+              market.outcomes.forEach((outcome) => {
+                if (outcome.name === homeTeam && (bestOdds.pointSpreadTeam1 === null || outcome.price > bestOdds.pointSpreadTeam1)) {
+                  bestOdds.pointSpreadTeam1 = outcome.price;
+                } else if (outcome.name === awayTeam && (bestOdds.pointSpreadTeam2 === null || outcome.price > bestOdds.pointSpreadTeam2)) {
+                  bestOdds.pointSpreadTeam2 = outcome.price;
+                }
+              });
+              break;
+            case 'totals':
+              market.outcomes.forEach((outcome) => {
+                if (outcome.name === 'Over' && (bestOdds.over === null || outcome.price > bestOdds.over)) {
+                  bestOdds.over = outcome.price;
+                } else if (outcome.name === 'Under' && (bestOdds.under === null || outcome.price > bestOdds.under)) {
+                  bestOdds.under = outcome.price;
+                }
+              });
+              break;
+            default:
+              break;
+          }
+        });
+      });
+
+      return bestOdds;
+    };
+
     const fetchOddsData = async () => {
       try {
-        const response = await axios.get('/api/nfl-odds');
-        setBestOdds(findBestOdds(response.data));
+        const response = await axios.get('https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?regions=us&oddsFormat=american&apiKey=62860a168ae81e9e9eaa2e8f832c75f0');
+        const odds = findBestOdds(response.data);
+        setBestOdds(odds);
       } catch (error) {
         console.error('Error fetching NFL odds:', error);
       }
     };
 
     fetchOddsData();
-  }, []); // Run this effect only once when the component mounts
+  }, [homeTeam, awayTeam, bookmakers]); // Include necessary dependencies for useEffect
 
-  // Function to find the best odds for each category
-  const findBestOdds = (oddsData) => {
-    const bestOdds = {
-      pointSpreadTeam1: null,
-      pointSpreadTeam2: null,
-      over: null,
-      under: null,
-      moneylineTeam1: null,
-      moneylineTeam2: null,
-    };
+  const renderOddsButtons = () => {
+    if (!bestOdds) {
+      return <p>Loading odds data...</p>;
+    }
 
-    // Loop through bookmakers
-    bookmakers.forEach((bookmaker) => {
-      // Loop through markets
-      bookmaker.markets.forEach((market) => {
-        switch (market.key) {
-          case 'h2h':
-            // Loop through outcomes
-            market.outcomes.forEach((outcome) => {
-              if (outcome.name === homeTeam && (bestOdds.moneylineTeam1 === null || outcome.price > bestOdds.moneylineTeam1)) {
-                bestOdds.moneylineTeam1 = outcome.price;
-              } else if (outcome.name === awayTeam && (bestOdds.moneylineTeam2 === null || outcome.price > bestOdds.moneylineTeam2)) {
-                bestOdds.moneylineTeam2 = outcome.price;
-              }
-            });
-            break;
-          case 'spreads':
-            // Loop through outcomes
-            market.outcomes.forEach((outcome) => {
-              if (outcome.name === homeTeam && (bestOdds.pointSpreadTeam1 === null || outcome.price > bestOdds.pointSpreadTeam1)) {
-                bestOdds.pointSpreadTeam1 = outcome.price;
-              } else if (outcome.name === awayTeam && (bestOdds.pointSpreadTeam2 === null || outcome.price > bestOdds.pointSpreadTeam2)) {
-                bestOdds.pointSpreadTeam2 = outcome.price;
-              }
-            });
-            break;
-          case 'totals':
-            // Loop through outcomes
-            market.outcomes.forEach((outcome) => {
-              if (outcome.name === 'Over' && (bestOdds.over === null || outcome.price > bestOdds.over)) {
-                bestOdds.over = outcome.price;
-              } else if (outcome.name === 'Under' && (bestOdds.under === null || outcome.price > bestOdds.under)) {
-                bestOdds.under = outcome.price;
-              }
-            });
-            break;
-          default:
-            break;
-        }
-      });
-    });
-
-    return bestOdds;
+    return (
+      <div className="odds-grid">
+        <button>Best Point Spread {awayTeam}: {bestOdds.pointSpreadTeam2}</button>
+        <button>Best Point Spread {homeTeam}: {bestOdds.pointSpreadTeam1}</button>
+        <button>Best Over: {bestOdds.over}</button>
+        <button>Best Under: {bestOdds.under}</button>
+        <button>Best Moneyline {awayTeam}: {bestOdds.moneylineTeam2}</button>
+        <button>Best Moneyline {homeTeam}: {bestOdds.moneylineTeam1}</button>
+      </div>
+    );
   };
 
   return (
@@ -80,18 +92,7 @@ const GameCard = ({ homeTeam, awayTeam, bookmakers }) => {
         <span>{homeTeam}</span>
       </div>
       <div className="best-odds">
-        {bestOdds ? (
-          <div>
-            <p>Best Point Spread {awayTeam}: {bestOdds.pointSpreadTeam2}</p>
-            <p>Best Point Spread {homeTeam}: {bestOdds.pointSpreadTeam1}</p>
-            <p>Best Over: {bestOdds.over}</p>
-            <p>Best Under: {bestOdds.under}</p>
-            <p>Best Moneyline {awayTeam}: {bestOdds.moneylineTeam2}</p>
-            <p>Best Moneyline {homeTeam}: {bestOdds.moneylineTeam1}</p>
-          </div>
-        ) : (
-          <p>Loading odds data...</p>
-        )}
+        {renderOddsButtons()}
       </div>
     </div>
   );
