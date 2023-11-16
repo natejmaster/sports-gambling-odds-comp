ef78f8a8b5b4140dcff87821053540417d28a219
Nate Master <natejmaster@gmail.com>
2023-11-15 18:19:30 -0600

    Revert "Replaced gamecard logic with API-driven widget"

---
+++ client/src/components/GameCards/index.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import GameCard from '../GameCard';
+
+
+export default function GameCards() {
+    const [gameData, setGameData] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?regions=us&oddsFormat=american&apiKey=62860a168ae81e9e9eaa2e8f832c75f0');
+          console.log(response.data);
+          setGameData(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData();
+    }, []); // Run this effect only once when the component mounts
+return (
+  <div className="app">
+  <h1>NFL Game Odds</h1>
+  <div className="game-cards">
+    {gameData.map((game) => (
+      <GameCard
+        key={game.id}
+        awayTeam={game.away_team}
+        homeTeam={game.home_team}
+        bookmakers={game.bookmakers}
+      />
+    ))}
+  </div>
+</div>
+)
+}
