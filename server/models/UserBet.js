44e0fb0fb8a588fde28a98d7e79587b84ce66287
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-16 20:42:41 -0600

    added user bets, wins, losses, and points earned to the userstatistics schema

--- server/models/UserBet.js
+++ server/models/UserBet.js
@@ -6,4 +6,20 @@ const UserStatisticsSchema = new mongoose.Schema({
         ref: 'User', // This will connect the userId to the correct user from User.js
         required: true,
     },
-})
+    totalBets: {
+        type: Number,
+        default: 0,
+    },
+    totalWins: {
+        type: Number,
+        default: 0,
+    },
+    totalLosses: {
+        type: Number,
+        default: 0,
+    },
+    totalPoints: {
+        type: Number,
+        default: 0,
+    },
+});
