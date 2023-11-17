dbcc6bfee00c10091a99e1f80e37178bcc6bac03
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-16 19:27:53 -0600

    adding handling connection events to display messages upon successful and unsuccessful attempts to connect to MongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,13 @@ const User = require('../models/User');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportsbetting', {
     userNewUrlParser: true,
     useUnifiedTopology: true,
+})
+
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(error => {
+    console.error('Could not connect to MongoDB:', error)
 });
 
 module.exports = mongoose.connection;
