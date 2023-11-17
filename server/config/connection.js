89d5db9c9601d38f57261725c1bc832f994e8248
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-16 20:00:49 -0600

    added more backend dependencies, also added temp user details in the connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,9 +5,26 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportsbet
     userNewUrlParser: true,
     useUnifiedTopology: true,
 })
-
-.then(() => {
+.then(async () => {
     console.log('Connected to MongoDB');
+
+    // We can replace this with actual user data once we're further along.
+    // Just temporary placeholder details.
+    const userDetails = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    };
+
+    const newUser = new User(userDetails);
+
+    try {
+        const savedUser = await newUser.save();
+        console.log('New user saved:', savedUser);
+    } catch (error) {
+        console.error('Error saving user:', error);
+    }
+    
 })
 .catch(error => {
     console.error('Could not connect to MongoDB:', error)
