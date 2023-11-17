b8afb97765e5695d27616f13a825f44f870db34a
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-16 19:24:01 -0600

    utiliziing usenewurlparser and useunifiedtopology to help avoid deprecation

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+const User = require('../models/User');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportsbetting');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportsbetting', {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
