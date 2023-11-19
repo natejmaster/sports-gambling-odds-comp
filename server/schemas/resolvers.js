6c458dca6156b9c0ec5b9c4f3489cb662bdb91a2
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-19 16:38:54 -0600

    added resolvers, typedefs, and front end mutations only for auth access

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,30 @@
 
 
 const resolvers = {
-
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+          const user = await User.create({ username, email, password });
+          const token = signToken(user);
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw AuthenticationError;
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw AuthenticationError;
+          }
+    
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+    },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,21 @@
 const typeDefs = `
   type User {
     _id: ID!
-    name: String!
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+type Query {
+  users: [User]
+}
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
 `;
 
