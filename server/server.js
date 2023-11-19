4c341cac8d03b099ea6bf3324f3734aef95fb41c
natejmaster@gmail.com <natejmaster@gmail.com>
2023-11-19 16:09:45 -0600

    Established connection between Bovada API and server.

--- server/server.js
+++ server/server.js
@@ -4,46 +4,35 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const axios = require('axios');
 
-const { typeDefs, resolvers } = require('./schemas');
+// const { typeDefs, resolvers } = require('./schemas'); // Commented out GraphQL setup
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// const server = new ApolloServer({ // Commented out GraphQL setup
+//   typeDefs,
+//   resolvers,
+// });
 
 const startApolloServer = async () => {
-  await server.start();
+  // await server.start(); // Commented out GraphQL setup
   
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   
-  app.use('/graphql', expressMiddleware(server));
+  // app.use('/graphql', expressMiddleware(server)); // Commented out GraphQL setup
 
   // New route for fetching NFL odds data
-  app.get('/api/nfl-odds', async (req, res) => {
+  app.get('/api/bovada-data', async (req, res) => {
     try {
-      const response = await axios.get(
-        'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/',
-        {
-          params: {
-            regions: 'us',
-            oddsFormat: 'american',
-            apiKey: '62860a168ae81e9e9eaa2e8f832c75f0',
-          },
-        }
-      );
-
-      const oddsData = response.data; // Adjust this based on the structure of the API response
-
-      res.json(oddsData);
+      const response = await axios.get('https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl');
+      res.json(response.data);
     } catch (error) {
-      console.error('Error fetching NFL odds:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.error(error); // Log the error
+      res.status(500).json({ error: error.message });
     }
   });
+  
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
@@ -57,7 +46,7 @@ const startApolloServer = async () => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      // console.log(`Use GraphQL at http://localhost:${PORT}/graphql`); // Commented out GraphQL setup
     });
   });
 };
