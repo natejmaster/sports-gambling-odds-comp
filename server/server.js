f4268327df4372644d8afb55c6f3b7de03f10760
natejmaster@gmail.com <natejmaster@gmail.com>
2023-11-19 20:57:12 -0600

    Added more data to simplified bovada data endpoint for ease of access on front end

--- server/server.js
+++ server/server.js
@@ -24,33 +24,68 @@ const startApolloServer = async () => {
 
   // app.use('/graphql', expressMiddleware(server)); //
 
+  //
+  app.get('/api/bovada-data', async (req, res) => {
+    try {
+      const response = await axios.get('https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl');
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
   // New route for fetching NFL odds data
   app.get('/api/simplified-bovada-data', async (req, res) => {
     try {
       const response = await axios.get('https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl');
-
-      // Log the full response to see what's coming from Bovada
-      console.log('Full Bovada API Response:', response.data);
-
-      // Extracting the "events" data and simplifying it
-      const eventsData = response.data[0]?.events || [];
-      console.log('Extracted Events Data:', eventsData);
-
-      const simplifiedData = eventsData.map((event) => {
+  
+      // Extracting detailed information for each event
+      const simplifiedData = response.data[0]?.events.map((event) => {
+        // Find the index of the desired market data by description
+        const findMarketIndex = (description) =>
+          event.displayGroups[0]?.markets.findIndex((market) => market.description === description);
+  
+        // Find the index of the point spread market
+        const pointSpreadIndex = findMarketIndex('Point Spread');
+  
+        // Extracting data for the underdog
+        const underdogOutcome = event.displayGroups[0]?.markets[pointSpreadIndex]?.outcomes[0];
+        const underdog = {
+          name: underdogOutcome?.description,
+          pointSpread: underdogOutcome?.price?.handicap,
+        };
+  
+        // Extracting data for the favorite
+        const favoriteOutcome = event.displayGroups[0]?.markets[pointSpreadIndex]?.outcomes[1];
+        const favorite = {
+          name: favoriteOutcome?.description,
+          pointSpread: favoriteOutcome?.price?.handicap,
+        };
+  
+        // Find the index of the over/under total market
+        const totalIndex = findMarketIndex('Total');
+  
+        // Extracting the total score
+        const totalScoreOutcome = event.displayGroups[0]?.markets[totalIndex]?.outcomes[0];
+        const totalScore = totalScoreOutcome?.price?.handicap;
+  
         return {
           matchup: event.description,
+          underdog,
+          favorite,
+          totalScore,
         };
       });
-
+  
       console.log('Simplified Data:', simplifiedData);
-
+  
       res.json(simplifiedData);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-
+  
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
