6dca5147e1e0ca9f5129f1773ac9d09a3c144ac6
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-20 19:03:29 -0600

    commiting to pull

--- client/src/components/GameCards/index.jsx
+++ client/src/components/GameCards/index.jsx
@@ -19,38 +19,38 @@ const MatchupsComponent = () => {
   }, []); // Empty dependency array to run the effect only once
 
   const renderMatchupButtons = (matchup) => (
-    <tr key={matchup.matchup}>
-      <td>{matchup.matchup}</td>
-      <td>
+    <tr key={matchup.matchup} className='border-blue'>
+      <td className='border-blue roaylBlue p-2'>{matchup.matchup}</td>
+      <td className='border-blue royalBlue p-2'>
         <button>
           {`${matchup.awayTeam.name} ${matchup.awayTeam.pointSpread > 0 ? `+${matchup.awayTeam.pointSpread}` : matchup.awayTeam.pointSpread}`}
         </button>
       </td>
-      <td>
+      <td className='border-blue royalBlue p-2'>
         <button>
           {`${matchup.homeTeam.name} ${matchup.homeTeam.pointSpread > 0 ? `+${matchup.homeTeam.pointSpread}` : matchup.homeTeam.pointSpread}`}
         </button>
       </td>
-      <td>
+      <td className='border-blue royalBlue p-2'>
         <button>{`Over ${matchup.totalScore}`}</button>
       </td>
-      <td>
+      <td className='border-blue royalBlue p-2'>
         <button>{`Under ${matchup.totalScore}`}</button>
       </td>
     </tr>
   );
 
   return (
-    <div>
-      <h2>Matchups</h2>
+    <div className="flex flex-col white-bg mt-4 mx-5 rounded-xl border-royalBlue items-center justify-center mb-32">
+      <h2 className="text-3xl heading">Matchups</h2>
       <table>
         <thead>
           <tr>
-            <th>Matchup</th>
-            <th>Away Team</th>
-            <th>Home Team</th>
-            <th>Over</th>
-            <th>Under</th>
+            <th className="text-lg royalBlue">Matchup</th>
+            <th className="text-lg royalBlue">Away Team</th>
+            <th className="text-lg royalBlue">Home Team</th>
+            <th className="text-lg royalBlue">Over</th>
+            <th className="text-lg royalBlue">Under</th>
           </tr>
         </thead>
         <tbody>
