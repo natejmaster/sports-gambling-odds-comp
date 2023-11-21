c0f58bd8f28dd1c2845cc011140d404ee65025af
natejmaster@gmail.com <natejmaster@gmail.com>
2023-11-20 19:36:28 -0600

    removed start time from UI

--- client/src/components/GameCards/index.jsx
+++ client/src/components/GameCards/index.jsx
@@ -17,44 +17,27 @@ const GameCards = () => {
     fetchData();
   }, []);
 
-  const renderMatchupButtons = (matchup) => {
-    const startTimeInMillis = matchup.startTime;
-    const startTimeDate = new Date(startTimeInMillis);
-
-    const options = {
-      timeZone: 'America/Chicago',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      hour12: true,
-    };
-    const formattedStartTime = startTimeDate.toLocaleDateString('en-US', options);
-
-    return (
-      <tr key={matchup.matchup}>
-        <td>{formattedStartTime}</td>
-        <td>{matchup.matchup}</td>
-        <td>
-          <button>
-            {`${matchup.awayTeam.name} ${matchup.awayTeam.pointSpread > 0 ? `+${matchup.awayTeam.pointSpread}` : matchup.awayTeam.pointSpread}`}
-          </button>
-        </td>
-        <td>
-          <button>
-            {`${matchup.homeTeam.name} ${matchup.homeTeam.pointSpread > 0 ? `+${matchup.homeTeam.pointSpread}` : matchup.homeTeam.pointSpread}`}
-          </button>
-        </td>
-        <td>
-          <button>{`Over ${matchup.totalScore}`}</button>
-        </td>
-        <td>
-          <button>{`Under ${matchup.totalScore}`}</button>
-        </td>
-      </tr>
-    );
-  };
+  const renderMatchupButtons = (matchup) => (
+    <tr key={matchup.matchup}>
+      <td>{matchup.matchup}</td>
+      <td>
+        <button>
+          {`${matchup.awayTeam.name} ${matchup.awayTeam.pointSpread > 0 ? `+${matchup.awayTeam.pointSpread}` : matchup.awayTeam.pointSpread}`}
+        </button>
+      </td>
+      <td>
+        <button>
+          {`${matchup.homeTeam.name} ${matchup.homeTeam.pointSpread > 0 ? `+${matchup.homeTeam.pointSpread}` : matchup.homeTeam.pointSpread}`}
+        </button>
+      </td>
+      <td>
+        <button>{`Over ${matchup.totalScore}`}</button>
+      </td>
+      <td>
+        <button>{`Under ${matchup.totalScore}`}</button>
+      </td>
+    </tr>
+  );
 
   return (
     <div>
@@ -62,7 +45,6 @@ const GameCards = () => {
       <table>
         <thead>
           <tr>
-            <th>Start Time (CT)</th>
             <th>Matchup</th>
             <th>Away Team</th>
             <th>Home Team</th>
