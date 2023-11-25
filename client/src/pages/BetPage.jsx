e11e7ae592c08e9476117bdba4bf6f70a813c952
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-24 18:46:31 -0600

    added spinner

--- client/src/pages/BetPage.jsx
+++ client/src/pages/BetPage.jsx
@@ -183,7 +183,7 @@ const BetPage = () => {
     <div className="flex flex-col white-bg mt-4 mx-5 rounded-xl border-royalBlue shadow-xl items-center justify-center mb-80">
       <h2 className="text-3xl heading">Matchups</h2>
 {loading && <div className="my-16 flex flex-row">
-  <p className="heading text-3xl">Loading...</p>
+  <p className="heading text-3xl">Loading Matchups...</p>
 <div className="heading text-3xl loading loading-spinner loading-lg"></div>
 </div>
 }
