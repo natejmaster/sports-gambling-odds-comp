a4f1755fedd3fc5f3576edebf27de1010d1a3ecf
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-28 13:55:11 -0600

    editing buy units react page

--- client/src/pages/BuyUnits.jsx
+++ client/src/pages/BuyUnits.jsx
@@ -1,7 +1,7 @@
 import '../index.css';
 export default function BuyUnits() {
     return (
-        <div>
+        <div id="parent-title">
             <h1 id="buy-units-title">Feel Like Pressing Your Luck? Buy More Units!</h1>
         </div>
     )
