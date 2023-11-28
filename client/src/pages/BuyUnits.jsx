f902f0e013db281bd77f0ab000b58ebffe12b26e
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-28 14:05:36 -0600

    adding payment options for the users

--- client/src/pages/BuyUnits.jsx
+++ client/src/pages/BuyUnits.jsx
@@ -1,19 +1,28 @@
 import '../index.css';
 export default function BuyUnits() {
-    return (
+    const paymentOptions = [
+        { price: 5, credits: 250 },
+        { price: 10, credits: 550 },
+        { price: 20, credits: 1250 },
+        // Add more payment options as needed
+      ];
+    
+      return (
         <div id="parent-title">
-            <h1 id="buy-units-title">Feel Like Pressing Your Luck? Buy More Units!</h1>
-            
-            <h2>Payment Options</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Payment Amount</th>
-                        <th>Credits Received</th>
-                    </tr>
-                </thead>
-            </table>
-
+          <h1 id="buy-units-title">Feel Like Pressing Your Luck? Buy More Units!</h1>
+          <div>
+      <h2>Select a Payment Option:</h2>
+      <div>
+        {paymentOptions.map((option, index) => (
+          <button
+            key={index}
+            onClick={() => handlePayment(option.price, option.credits)}
+          >
+            {`Pay $${option.price} - ${option.credits} credits`}
+          </button>
+        ))}
+      </div>
+    </div>
         </div>
-    )
+      );
 }
