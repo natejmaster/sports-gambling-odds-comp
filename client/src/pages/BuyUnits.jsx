b719f7d9b71f057b6982c4c1b2fb03fd0b48d41a
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-28 13:58:59 -0600

    creating a payment table to display options for users

--- client/src/pages/BuyUnits.jsx
+++ client/src/pages/BuyUnits.jsx
@@ -3,6 +3,17 @@ export default function BuyUnits() {
     return (
         <div id="parent-title">
             <h1 id="buy-units-title">Feel Like Pressing Your Luck? Buy More Units!</h1>
+            
+            <h2>Payment Options</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Payment Amount</th>
+                        <th>Credits Received</th>
+                    </tr>
+                </thead>
+            </table>
+
         </div>
     )
 }
