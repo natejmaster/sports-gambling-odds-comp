fcd8cae873f761afcbe65b33897b423ed8b3645a
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-11-28 20:55:40 -0600

    fixed errors and revert

--- client/src/pages/LeaderBoard.jsx
+++ client/src/pages/LeaderBoard.jsx
@@ -17,24 +17,24 @@ export default function LeaderBoard() {
 console.log(users);
     const username = Auth.getProfile().data.username;
     return (
-       <>
-       <div className="flex flex-col  white-bg mx-5 mt-5 py-2 rounded-xl border-royalBlue  mb-32 justify-center">
-            <h2 className="heading text-3xl text-center">Leaderboard</h2>
-            <h3 className="heading text-3xl text-center">Top 10</h3>
-            <ol className="flex flex-col list-decimal">
-           {users.map((user, index) => {
-                return (
-                    
-                        <li key={user._id} className="flex flex-col justify-center items-center border-royalBlueTop ">
-                        <h3 className={username === user.username ? ("heading text-2xl font-bold text-center") : "text-xl font-bold text-center"}>
-                       <h3 className="heading text-2xl font-bold"> {index === 0 ? '1st' : (index === 1 ? '2nd' : (index === 2 ? '3rd' : (index >= 3 && index <= 10 ? `${index + 1}th` : '')))}</h3> {user.username}
-                        </h3>
-                        <p className="royalBlue font-bold">{user.units} units</p></li>
-                     
-                        
-                )
-            })}   </ol>
-            </div>
-       </>
-    )
+<>
+  <div className="flex flex-col white-bg mx-5 mt-5 py-2 rounded-xl border-royalBlue mb-32 justify-center">
+    <h2 className="heading text-3xl text-center">Leaderboard</h2>
+    <h3 className="heading text-3xl text-center">Top 10</h3>
+    <ol className="flex flex-col list-decimal">
+      {users.map((user, index) => (
+        <li key={user._id} className="flex flex-col justify-center items-center border-royalBlueTop">
+          <p className={username === user.username ? "heading text-2xl font-bold text-center" : "text-xl font-bold text-center"}>
+            {index === 0 ? '1st' : (index === 1 ? '2nd' : (index === 2 ? '3rd' : (index >= 3 && index <= 10 ? `${index + 1}th` : '')))} {user.username}
+          </p>
+          <p className="royalBlue font-bold">{user.units} units</p>
+        </li>
+      ))}
+    </ol>
+  </div>
+</>
+
+    );
 }
+
+
